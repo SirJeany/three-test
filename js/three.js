314d3a3d6f8ecfa99b65c613ef549bba5508a8d8
SirJeany <jmvanriet1@gmail.com>
2020-03-19 09:23:29 +0200

    Small changes in js. tried canvas ele in html, didnt work

--- js/three.js
+++ js/three.js
@@ -1,10 +1,15 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// Use the following in order to determine width and height of window: (we want the size of the element in html)
+const animationSection = document.getElementById('animationSection'); //using this element to get it's width and height.
+// let windowWidth = ;
+// let windowHeight = ;
 
-const animationSection = document.getElementById('animationSection');
+var camera = new THREE.PerspectiveCamera( 25, window.innerWidth/window.innerHeight, 0.1, 100 );
+
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth/2, window.innerHeight/2 ); // div by 2 to get half size.
+
 animationSection.appendChild( renderer.domElement )
 // document.body.appendChild( renderer.domElement );
 
